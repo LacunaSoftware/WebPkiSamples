5a8e56a86a872f170d70414689845d5c3beae53c
Ismael Medeiros <140083162@aluno.unb.br>
2018-05-23 17:12:08 -0300

    Fixed provided public folder logic.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-app.use('/', express.static(path.join(__dirname, 'webapp')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Error handler
 app.use(function(err, req, res, next) {
